d5cdd786aa0a815ea962cce9d1b6534dd5558cc7
macae craque de bola <128196105+macaecraquedebola10@users.noreply.github.com>
2024-11-26 00:28:16 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector("h1") 
+titulo.textContent = "vinicius nutricionista";
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "pacientes cadastrados"; 
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
